a9ef3adf4aacf192bc1f2ecbe9462f6235130e57
Hosein Emdadi <hemdadi71@gmail.com>
2023-02-08 17:35:41 +0330

    question/1 compeleted

--- question-1/app.js
+++ question-1/app.js
@@ -10,3 +10,17 @@ function setterGenerator(key) {
 const nameSetter = setterGenerator('name');
 nameSetter('Jack');
 console.log(user);
+
+// .....................................................
+
+// const user = {};
+
+// function setterGenerator(key) {
+//   return function (value) {
+//     this[key] = value;
+//     return user;
+//   };
+// }
+// const nameSetter = setterGenerator('name');
+// nameSetter.call(user, 'Jack');
+// console.log(user);
